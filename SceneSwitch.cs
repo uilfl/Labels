9c39b521943b94205768e4cc2d502d07b5ee2e08
Yusheng Chen <80140640+uilfl@users.noreply.github.com>
2024-08-01 21:13:12 +0800

    Add files via upload
    This is the scripts for our project

---
+++ SceneSwitch.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    public int butterfliesCount = 0; // 觸碰籤詩的數量
+    public ButterflyGenerator butterflyGenerator;
+
+
+     void Start()
+    {
+        if (butterflyGenerator == null)
+        {
+            butterflyGenerator = FindObjectOfType<ButterflyGenerator>();
+        }
+    }
+
+
+    public void CountButterflies()
+    {
+        butterfliesCount++;
+    }
+
+    public void OnTriggerEnter(Collider butterflies)
+    {
+        if (butterfliesCount >= 5)
+        {
+            SceneManager.LoadScene(butterflies.gameObject.GetComponent<SceneInfo>().SceneName); // 載入下一個場景
+        }
+        else
+        {
+            CountButterflies();
+        }
+    }
+        
+}
